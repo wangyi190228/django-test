f14a5bef7a4577e2343641587c787fde2ddd98c1
wangyitest <1204534516@qq.com>
2019-08-06 17:40:38 +0800

    the newest code

--- attend/views.py
+++ attend/views.py
@@ -1,75 +1,120 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 # my code region
 from django.http import HttpResponseRedirect
 from .forms import UploadFileForm
-import xlrd
-import xlwt
-from io import StringIO
-
+import xlrd,xlwt
 # my attend function
 
-
+listlen = 0
 def attend(request):
     attendlist = []
     templist=[]
-    tempflag = False
+    tempflag = False   
     if request.method == 'POST':
-        form = UploadFileForm(request.POST,request.FILES)
-        if form.is_valid():
-            f = request.FILES['file']
-            data = xlrd.open_workbook(filename=None,file_contents=f.read(), encoding_override='utf-8')
-            table = data.sheets()[0]
-            nrows = table.nrows
-            for i in range(1,nrows):
-                tempflag =True
-                templist.clear()
-                templist.append(table.row_values(i))
-                if templist[0][1] == '':
-                    continue
-                else:
-                    listlen = len(attendlist)
-                    if listlen==0:
-                        attendlist.append(table.row_values(i))
-                        attendlist.append(table.row_values(i))
-                    else:
-                        for index in range(0,listlen,2):
-                            if templist[0][0].split(' ')[0] == attendlist[index][0].split(' ')[0] and templist[0][1] != attendlist[index][1]:
-                                continue
-                            elif templist[0][0].split(' ')[0] == attendlist[index][0].split(' ')[0] and templist[0][1] == attendlist[index][1]:
-                                tempflag = False
-                                if templist[0][0] < attendlist[index][0]:
-                                    attendlist[index] = templist[0]
-                                elif templist[0][0] > attendlist[index+1][0]:
-                                    attendlist[index+1] = templist[0]
-                            elif templist[0][0].split(' ')[0] != attendlist[index][0].split(' ')[0]:
-                                tempflag = True
-                                continue
-                        if tempflag:
-                            attendlist.append(table.row_values(i))
-                            attendlist.append(table.row_values(i))
-                            
-            listlen = len(attendlist)
-            for index in range((listlen-1),-1,-2):
-                date = attendlist[index-1][0].split(' ')[0]
-                time = attendlist[index-1][0].split(' ')[1]
-                attendlist[index-1].insert(0,date)
-                attendlist[index-1][1]=time
-                time = attendlist[index][0].split(' ')[1]
-                attendlist[index-1].insert(2,time)
-                attendlist.pop(index)
-            
-
+        if 'exsubmit' in request.POST:
+            listlen = len(attendlist) + 1
+            # if listlen >1:
             workbook = xlwt.Workbook(encoding = 'utf-8')
             # 创建一个worksheet
             worksheet = workbook.add_sheet('My Worksheet')
             # 写入excel
             # 参数对应 行, 列, 值
-            worksheet.write(1,0, label = 'this is test')
+            worksheet.write(0,0, 'Item')
+            worksheet.write(0,1, 'Date')
+            worksheet.write(0,2, 'Start Time')
+            worksheet.write(0,3, 'End Time')
+            worksheet.write(0,4, 'Afnumofperson')
+            worksheet.write(0,5, 'Name')
+            worksheet.write(0,6, 'Cardnum')
+            worksheet.write(0,7, 'Machinename')
+            worksheet.write(0,8, 'Event Place')
+            worksheet.write(0,9, 'Verify')
+            worksheet.write(0,10, 'Status')
+            worksheet.write(0,11, 'Type')
+            worksheet.write(0,12, 'Backup')
+            for rindex in range(1,listlen):
+                worksheet.write(rindex,0,rindex)
+                for cindex in range(1,13):
+                    worksheet.write(rindex,cindex, attendlist[rindex-1][cindex-1])
             # 保存
             workbook.save('Excel_test.xls')
-
-            return render(request, 'test.html',{'attendlist':attendlist,'form': form})
+            return redirect('/attend/')
+            # form = UploadFileForm()
+            # return redirect('/attend/')
+        else:
+            listlen = 0
+            form = UploadFileForm(request.POST,request.FILES)
+            if form.is_valid():
+                f = request.FILES['file']
+                data = xlrd.open_workbook(filename=None,file_contents=f.read(), encoding_override='utf-8')
+                table = data.sheets()[0]
+                nrows = table.nrows
+                for i in range(1,nrows):
+                    tempflag =True
+                    templist.clear()
+                    templist.append(table.row_values(i))
+                    if templist[0][1] == '':
+                        continue
+                    else:
+                        listlen = len(attendlist)
+                        if listlen==0:
+                            attendlist.append(table.row_values(i))
+                            attendlist.append(table.row_values(i))
+                        else:
+                            for index in range(0,listlen,2):
+                                if templist[0][0].split(' ')[0] == attendlist[index][0].split(' ')[0] and templist[0][1] != attendlist[index][1]:
+                                    continue
+                                elif templist[0][0].split(' ')[0] == attendlist[index][0].split(' ')[0] and templist[0][1] == attendlist[index][1]:
+                                    tempflag = False
+                                    if templist[0][0] < attendlist[index][0]:
+                                        attendlist[index] = templist[0]
+                                    elif templist[0][0] > attendlist[index+1][0]:
+                                        attendlist[index+1] = templist[0]
+                                elif templist[0][0].split(' ')[0] != attendlist[index][0].split(' ')[0]:
+                                    tempflag = True
+                                    continue
+                            if tempflag:
+                                attendlist.append(table.row_values(i))
+                                attendlist.append(table.row_values(i))
+                                
+                listlen = len(attendlist)
+                for index in range((listlen-1),-1,-2):
+                    date = attendlist[index-1][0].split(' ')[0]
+                    time = attendlist[index-1][0].split(' ')[1]
+                    attendlist[index-1].insert(0,date)
+                    attendlist[index-1][1]=time
+                    time = attendlist[index][0].split(' ')[1]
+                    attendlist[index-1].insert(2,time)
+                    attendlist.pop(index)
+                return render(request, 'test.html',{'attendlist':attendlist,'form': form})
     else:
+
+        listlen = len(attendlist) + 1
+        workbook = xlwt.Workbook(encoding = 'utf-8')
+        # 创建一个worksheet
+        worksheet = workbook.add_sheet('My Worksheet')
+        # 写入excel
+        # 参数对应 行, 列, 值
+        worksheet.write(0,0, 'Item')
+        worksheet.write(0,1, 'Date')
+        worksheet.write(0,2, 'Start Time')
+        worksheet.write(0,3, 'End Time')
+        worksheet.write(0,4, 'Afnumofperson')
+        worksheet.write(0,5, 'Name')
+        worksheet.write(0,6, 'Cardnum')
+        worksheet.write(0,7, 'Machinename')
+        worksheet.write(0,8, 'Event Place')
+        worksheet.write(0,9, 'Verify')
+        worksheet.write(0,10, 'Status')
+        worksheet.write(0,11, 'Type')
+        worksheet.write(0,12, 'Backup')
+        for rindex in range(1,listlen):
+            worksheet.write(rindex,0,rindex)
+            for cindex in range(1,13):
+                worksheet.write(rindex,cindex, attendlist[rindex-1][cindex-1])
+        # 保存
+        workbook.save('Excel_test.xls')
+        # return redirect('/attend/')
         form = UploadFileForm()
         return render(request, 'test.html',{'form': form})
 
