63a0d7e6319ab6358e9debd0788f2b22a9fce95f
wangyi190 <1204534516@qq.com>
2019-08-08 23:04:07 +0800

    the newest code

--- static/js/afsty.js
+++ static/js/afsty.js
@@ -1,23 +1,36 @@
 $(document).ready(function(){
+
+    // $(".tabs").tabs();
+
+    $(".alli1").css("background-color","#6495ED");
+
     $("#albtn1").on("click", function () {
+        $(".alli1").css("background-color","#6495ED");
+        $(".alli2").css("background-color","#98F5FF");
+        $(".alli3").css("background-color","#98F5FF");
         $.get( "/annualdays/", function( data ) {
             $( "#body-content" ).html( data );
           });
     });
 
     $("#albtn2").on("click", function () {
+        $(".alli1").css("background-color","#98F5FF");
+        $(".alli2").css("background-color","#6495ED");
+        $(".alli3").css("background-color","#98F5FF");
         $.get( "/annuallist/", function( data ) {
             $( "#body-content" ).html( data );
           });
     });
 
     $("#albtn3").on("click", function () {
+        $(".alli1").css("background-color","#98F5FF");
+        $(".alli2").css("background-color","#98F5FF");
+        $(".alli3").css("background-color","#6495ED");
         $.get( "/approvallist/", function( data ) {
             $( "#body-content" ).html( data );
           });
     });
 
-    // $(".tabs").tabs();
 });
 
 
