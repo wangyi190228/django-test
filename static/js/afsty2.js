ed6dc4a8224b16ba465d9dd1db3cff30e9a5c9c6
wangyitest <1204534516@qq.com>
2019-08-28 18:37:47 +0800

    the new code

--- static/js/afsty2.js
+++ static/js/afsty2.js
@@ -6,4 +6,24 @@ $(document).ready(function () {
         "padding":"5px",
     });
 
+    $(".subapp").button().css({
+        "width":"80px",
+        "margin-left":"40px",
+        "background-color":"#98F5FF",
+        "padding":"5px",  
+    });
+
+    $("#startin").datepicker({
+        minDate:0,
+        onClose:function(datatext){
+            $("#stopin").datepicker("option","minDate",datatext);
+        },
+    });
+    $("#stopin").datepicker({
+        minDate:0,
+        onClose:function(datatext){
+            $("#startin").datepicker("option","maxDate",datatext);
+        },
+    });
+
 });
